d5b0d8dc7c678222b41c2e22a1c781df12f2495c
Rui <464362353@qq.com>
2017-07-30 18:21:18 +0800

    设置布局

---
+++ src/components/Topbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="topbar">
+    <div class="wrapper">
+      <span class="logo"> Resumer </span>
+    </div>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'Topbar'
+  }
+</script>
+
+<style lang="scss">
+  #topbar {
+    background: #fff;
+    box-shadow: 0 1px 3px 0 rgba(0,0,0,0.3);
+    > .wrapper {
+      min-width: 1024px;
+      max-width: 1440px;
+      margin: 0 auto;
+      height: 64px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 16px;
+      > .logo {
+        font-size: 24px;
+        color: #000;
+      }
+    }
+  }
+</style>
