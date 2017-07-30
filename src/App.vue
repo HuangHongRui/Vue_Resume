e4850f05a28250422c9ef6573fef5d3a953bba24
Rui <464362353@qq.com>
2017-07-30 15:09:28 +0800

    npm init & vue init webpack & 测试

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <p>你好</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: function () {
+    return {
+      text: '你好'
+    }
+  }
+}
+</script>
+
+<style>
+  p {color: red;}
+</style>
